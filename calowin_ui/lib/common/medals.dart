d32e3c25a62fd740b7076d57b1a6c2dbd6570040
RaphelSim <raphelsim@gmail.com>
2024-10-20 22:56:55 +0800

    Added platinum medals

--- calowin_ui/lib/common/medals.dart
+++ calowin_ui/lib/common/medals.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Medals {
+  static Image caloriePlatinum = Image.asset(
+    'assets/images/CaloriePlatinum.png',
+    fit: BoxFit.contain,
+  );
   static Image calorieGold = Image.asset(
     'assets/images/CalorieGold.png',
     fit: BoxFit.contain,
@@ -9,8 +13,12 @@ class Medals {
       Image.asset('assets/images/CalorieSilver.png', fit: BoxFit.contain);
   static Image calorieBronze =
       Image.asset('assets/images/CalorieBronze.png', fit: BoxFit.contain);
+
+  static Image ecoPlatinum =
+      Image.asset('assets/images/EcoPlatinum.png', fit: BoxFit.contain);
   static Image ecoGold =
       Image.asset('assets/images/EcoGold.png', fit: BoxFit.contain);
+
   static Image ecoSilver =
       Image.asset('assets/images/EcoSilver.png', fit: BoxFit.contain);
   static Image ecoBronze =
