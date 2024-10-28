f26be4f73e7f2d81008ea6a3c2642bbe45838cb0
CalowinVM <calowin@ntu.vm.com>
2024-10-29 05:22:31 +0800

    debug

--- WellnessZone/src/main/java/com/CalowinWellnessZone.java
+++ WellnessZone/src/main/java/com/CalowinWellnessZone.java
@@ -3,9 +3,11 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.WellnessZone" })
+@SpringBootApplication
 public class CalowinWellnessZone {
+
     public static void main(String[] args) {
         SpringApplication.run(CalowinWellnessZone.class, args);
     }
+
 }
