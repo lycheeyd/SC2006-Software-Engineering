3cff35b40823f154feaa45982cb55df57c3c187d
RaphelSim <raphelsim@gmail.com>
2024-10-31 02:11:40 +0800

    fixed calowin_ui pubspec.lock

--- calowin_ui/lib/control/park_retriever.dart
+++ calowin_ui/lib/control/park_retriever.dart
@@ -28,6 +28,7 @@ class ParkRetriever {
   // Function to retrieve parks based on user's coordinates
   Future<List<Park>> retrievePark(double userLat, double userLon) async {
     final url = Uri.parse('$_baseUrl?lat=$userLat&lon=$userLon');
+    //print(url);
     //final url = Uri.parse('$_baseUrl/$userLat/$userLon');
     try {
       final response = await http.get(url);
