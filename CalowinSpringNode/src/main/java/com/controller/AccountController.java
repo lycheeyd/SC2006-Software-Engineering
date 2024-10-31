708be35b2786617268c3fb808d67174e286f8479
lycheeyd <165633195+lycheeyd@users.noreply.github.com>
2024-10-31 21:27:43 +0800

    Fixed some bug

--- CalowinSpringNode/src/main/java/com/controller/AccountController.java
+++ CalowinSpringNode/src/main/java/com/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
