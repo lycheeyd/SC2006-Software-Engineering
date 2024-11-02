f320b9a88339cde001834b1204dafb96df632268
lycheeyd <165633195+lycheeyd@users.noreply.github.com>
2024-11-03 00:37:22 +0800

    Fixed android flutter

--- CalowinAccount/src/main/java/com/Account/HttpReqController.java
+++ CalowinAccount/src/main/java/com/Account/HttpReqController.java
@@ -86,7 +86,7 @@ public class HttpReqController {
     }
 
     @PostMapping("/send-otp")
-    public ResponseEntity<?> sendOTP(@RequestParam SendOtpDTO sendOtpDTO) {
+    public ResponseEntity<?> sendOTP(@RequestBody SendOtpDTO sendOtpDTO) {
         try {
             // Send the OTP
             otpService.sendOtpCode(sendOtpDTO.getEmail(), sendOtpDTO.getType());
@@ -100,7 +100,7 @@ public class HttpReqController {
     }
 
     @PostMapping("/verify-otp")
-    public ResponseEntity<?> verifyOTP(@RequestParam VerifyOtpDTO verifyOtpDTO) {
+    public ResponseEntity<?> verifyOTP(@RequestBody VerifyOtpDTO verifyOtpDTO) {
         try {
             // Verify the OTP
             if (!otpService.verifyOTP(verifyOtpDTO.getEmail(), verifyOtpDTO.getOtpCode(), verifyOtpDTO.getType())) {
