3acbc0ae5ce0ba39d5ef39a3a2c7a33e1bc93655
lycheeyd <165633195+lycheeyd@users.noreply.github.com>
2024-11-04 14:46:54 +0800

    Attempt to fix bug

--- CalowinSpringNode/src/main/java/com/CalowinSpringNode.java
+++ CalowinSpringNode/src/main/java/com/CalowinSpringNode.java
@@ -1,12 +1,19 @@
 package com;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.reactive.ClientHttpRequest;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 import com.config.CustomResponseErrorHandler;
+
+import io.micrometer.common.lang.Nullable;
 
 
 @SpringBootApplication
@@ -17,8 +24,19 @@ public class CalowinSpringNode {
 
     // For forwarding https request
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate customRestTemplate(RestTemplateBuilder builder) {
+        return builder.errorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                // Return false to prevent RestTemplate from throwing exceptions
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                // No handling here, just let the response be handled directly in the controller
+            }
+        }).build();
     }
 /* 
     @Bean
