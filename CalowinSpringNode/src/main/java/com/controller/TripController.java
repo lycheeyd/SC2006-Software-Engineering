77bca536b9b28b11a2034c1954eb6fe4765d1a01
CalowinVM <calowin@ntu.vm.com>
2024-11-06 18:05:30 +0800

    Upload unsaved changes

--- CalowinSpringNode/src/main/java/com/controller/TripController.java
+++ CalowinSpringNode/src/main/java/com/controller/TripController.java
@@ -1,14 +1,16 @@
 package com.controller;
 
-import java.util.Map;
-
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.DataTransferObject.TripDTO.AchievementMatrixDTO;
