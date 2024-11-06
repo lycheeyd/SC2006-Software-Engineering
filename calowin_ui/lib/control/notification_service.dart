09221060b2b06b57dc676fb42a54f8fddb91a459
Guan Xiaoxi <xguan007@e.ntu.edu.sg>
2024-11-07 03:23:10 +0800

    Noti

--- calowin_ui/lib/control/notification_service.dart
+++ calowin_ui/lib/control/notification_service.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class NotificationService {
-  final String baseUrl = "http://localhost:8084"; // Update with actual base URL
+  final String baseUrl = "http://localhost:8080"; // Update with actual base URL
 
   Future<List<String>> fetchFriendRequests(String userId) async {
-    final response = await http.get(Uri.parse('$baseUrl/notifications/friend-requests/$userId'));
+    final response = await http.get(Uri.parse('$baseUrl/api/user/$userId/friend-requests'));
 
     if (response.statusCode == 200) {
       // Parse the JSON response
