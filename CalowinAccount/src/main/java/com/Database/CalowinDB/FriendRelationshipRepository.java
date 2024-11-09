62961ad885cde87e0a9b4f15f22919254fee5c66
lycheeyd <165633195+lycheeyd@users.noreply.github.com>
2024-11-09 15:30:09 +0800

    Update database handling

--- CalowinAccount/src/main/java/com/Database/CalowinDB/FriendRelationshipRepository.java
+++ CalowinAccount/src/main/java/com/Database/CalowinDB/FriendRelationshipRepository.java
@@ -3,6 +3,9 @@ package com.Database.CalowinDB;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.Account.Entities.FriendRelationshipEntry;
@@ -10,5 +13,8 @@ import com.Account.Entities.FriendRelationshipEntry;
 @Repository
 public interface FriendRelationshipRepository extends JpaRepository<FriendRelationshipEntry, String> {
     Optional<FriendRelationshipEntry> findByUserID(String userID);
-    void deleteByUserID(String userID);
+
+    @Modifying
+    @Query("DELETE FROM FriendRelationshipEntry f WHERE f.Unique_ID = :Unique_ID")
+    void deleteByUserID(@Param("Unique_ID") String userID);
 }
